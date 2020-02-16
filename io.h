0a5473707729707f47b41acb2d5b7db732be47c2
John Lonergan <john.lonergan@gmail.com>
2020-02-16 01:12:20 +0000

    added more macro args

--- io.h
+++ io.h
@@ -29,23 +29,13 @@ OR use a block like ...
 
   */
 
-#define PASS   F("PASS   : ")
-#define FAIL   F("FAIL   : ")
-#define ERROR  F("ERROR  : ")
-#define INFO   F("INFO   : ")
-#define RESULT F("RESULT : ")
-#define HALT   F("HALT   : ")
-#define MATCH  F("MATCH  : ")
-
-//
-//void INFOLN(const char * s1, const __FlashStringHelper* s2);
-//void INFOLN(const char * s1, const char * s2="", const char * s3="", const char * s4="", const char * s5="");
-
-//void errorln(const char * s1, const char * s2="", const char * s3="", const char * s4="", const char * s5="");
-//void halt(const char* m1, const char* m2 = "", const char* m3 = "", const char* m4 = "", const char* m5 = "");
-
-//void print(const __FlashStringHelper* s1, const char * s2="", const char * s3="", const char * s4="", const char * s5="", const char * s6="", const char * s7="");
-//void println(const __FlashStringHelper* s1, const char * s2="", const char * s3="", const char * s4="", const char * s5="", const char * s6="", const char * s7="");
+#define S_PASS   F("PASS   : ")
+#define S_FAIL   F("FAIL   : ")
+#define S_ERROR  F("ERROR  : ")
+#define S_INFO   F("INFO   : ")
+#define S_RESULT F("RESULT : ")
+#define S_HALT   F("HALT   : ")
+#define S_MATCH  F("MATCH  : ")
 
 #define PRINT1(s1) \
   {Serial.print(s1); Serial.flush();}
@@ -89,7 +79,26 @@ OR use a block like ...
   Serial.print(s6); \
   Serial.print(s7); Serial.flush();}
 
+#define PRINT8(s1,s2,s3,s4,s5,s6,s7,s8) \
+  {Serial.print(s1); \
+  Serial.print(s2); \
+  Serial.print(s3); \
+  Serial.print(s4); \
+  Serial.print(s5); \
+  Serial.print(s6); \
+  Serial.print(s7); \
+  Serial.print(s8); Serial.flush();}
 
+#define PRINT9(s1,s2,s3,s4,s5,s6,s7,s8,s9) \
+  {Serial.print(s1); \
+  Serial.print(s2); \
+  Serial.print(s3); \
+  Serial.print(s4); \
+  Serial.print(s5); \
+  Serial.print(s6); \
+  Serial.print(s7); \
+  Serial.print(s8); \
+  Serial.print(s9); Serial.flush();}
 
 // SEE https://stackoverflow.com/questions/3046889/optional-parameters-with-c-macros
 // The multiple macros that you would need anyway [as per: Crazy Eddie]
@@ -100,13 +109,17 @@ OR use a block like ...
 #define XXX_4(T,A,B,C,D)              PRINT5(T,A,B,C,D)
 #define XXX_5(T,A,B,C,D,E)            PRINT6(T,A,B,C,D,E)
 #define XXX_6(T,A,B,C,D,E,F)          PRINT7(T,A,B,C,D,E,F)
+#define XXX_7(T,A,B,C,D,E,F,G)        PRINT8(T,A,B,C,D,E,F,G)
+#define XXX_8(T,A,B,C,D,E,F,G,H)      PRINT9(T,A,B,C,D,E,F,G,H)
 
 
 // The interim macro that simply strips the excess and ends up with the required macro
-#define XXX_X(x,A,B,C,D,E,FUNC, ...)  FUNC  
+#define XXX_X(x,A,B,C,D,E,F,G,FUNC, ...)  FUNC  
 
 // The macro that the programmer uses 
 #define XXX_GEN(CODE, ...)                XXX_X(,##__VA_ARGS__,\
+                                          XXX_8(CODE, __VA_ARGS__, "\n"),\
+                                          XXX_7(CODE, __VA_ARGS__, "\n"),\
                                           XXX_6(CODE, __VA_ARGS__, "\n"),\
                                           XXX_5(CODE, __VA_ARGS__, "\n"),\
                                           XXX_4(CODE, __VA_ARGS__, "\n"),\
@@ -115,11 +128,11 @@ OR use a block like ...
                                           XXX_1(CODE, __VA_ARGS__, "\n")\
                                          ) 
 
-#define ERRORLN(...)   XXX_GEN(ERROR, ##__VA_ARGS__)
-#define INFOLN(...)  XXX_GEN(INFO, ##__VA_ARGS__)
-#define HALTLN(...)   {XXX_GEN(INFO, ##__VA_ARGS__); while (1) delay(10000);}
-#define MATCHLN(...)   {XXX_GEN(MATCH, ##__VA_ARGS__);}
-#define FAILLN(...)  {XXX_GEN(FAIL, ##__VA_ARGS__);}
-#define PASSLN(...)   {XXX_GEN(PASS, ##__VA_ARGS__);}
+#define ERRORLN(...)   XXX_GEN(S_ERROR, ##__VA_ARGS__)
+#define INFOLN(...)  XXX_GEN(S_INFO, ##__VA_ARGS__)
+#define HALTLN(...)   {XXX_GEN(S_INFO, ##__VA_ARGS__); while (1) delay(10000);}
+#define MATCHLN(...)   {XXX_GEN(S_MATCH, ##__VA_ARGS__);}
+#define FAILLN(...)  {XXX_GEN(S_FAIL, ##__VA_ARGS__);}
+#define PASSLN(...)   {XXX_GEN(S_PASS, ##__VA_ARGS__);}
 
 #endif
