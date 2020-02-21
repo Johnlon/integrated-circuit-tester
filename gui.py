5944fed6baf82f8956048ff125468e494f5b0c80
John Lonergan <john.lonergan@gmail.com>
2020-02-21 20:18:15 +0000

    added pattern field

--- gui.py
+++ gui.py
@@ -4,9 +4,6 @@ import functools
 from tkinter.ttk import *
 
 
-def hello(event, param):
-    print("Single Click, Button-l %s" % (param))
-
 def quit(event):
     print("Double Click, so let's stop")
     import sys; sys.exit()
@@ -33,25 +30,26 @@ class Zif(Frame):
 
     zifPosX = 100
     zifPosY = 0
+    options = ["?","0","1","C","L","H","Z","X","S"]
 
     def __init__(self, parent, *args, **kwargs):
+        self.pinList = []
+        self.pinListIdx = []
+        self.testPattern=StringVar()
+
         tk.Frame.__init__(self, parent)
         self.initUI()
 
-    def initUI(self):
 
-        self.pinList = []
+    def initUI(self):
 
         self.config(height=Zif.height, width=Zif.width, background="orange", borderwidth=0, highlightthickness=0)
 
         canvasTop = Canvas(self, height=Zif.height+50, width=Zif.width*2, background="red", borderwidth=0, highlightthickness=0)
         canvasTop.pack(side=LEFT, padx=10, pady=20)
 
-        buttonStyle = Style()
-        buttonStyle.configure('W.TButton', font = ('calibri', 10, 'bold'), background='yellow')
-
-        labelStyle = Style()
-        labelStyle.configure('W.TLabel', font = ('calibri', 10, 'italic'), background='yellow')
+        style = Style()
+        style.configure('W.TButton', font = ('courier', 9, ''), background='yellow')
 
         canvas1 = Canvas(canvasTop, height=Zif.height, width=Zif.socketWidth,  background="yellow", borderwidth=1, highlightthickness=0)
         canvas1.place(x=Zif.zifPosX, y=Zif.zifPosY)
@@ -59,15 +57,20 @@ class Zif(Frame):
         canvas1.create_rectangle(20, 20, 40, 80, width=1, outline="black", fill="#EEE");
         canvas1.create_rectangle(18, 5, 42, 30, width=1, outline="black", fill="#EEE");
 
+        (f, self.patternField) = self.entry(canvasTop, font=("courier", 9), text="????????????/????????????", width=Zif.width, height=Zif.pinSize, textvariable=self.testPattern)
+        f.place(x=Zif.zifPosX, y=Zif.height+20)
+
         for pin in range(0, 24):
-            (widget,l) = self.label(canvasTop, text=str(pin+1), width=Zif.pinSize, height=Zif.pinSize, style='W.TLabel')
+            (widget,l) = self.label(canvasTop, text=str(pin+1), width=Zif.pinSize, height=Zif.pinSize, style='W.TButton')
             widget.place(x=self.pinPosH(pin), y=self.pinPosV(pin))
+            self.pinListIdx.append(0)
             self.pinList.append(l)
 
             (widget, b) = self.button(canvasTop, text="p"+str(pin+1), width=Zif.pinSize, height=Zif.pinSize, style='W.TButton')
             widget.place(x=self.buttonPosH(pin), y=self.pinPosV(pin))
-            b.bind('<Button-1>', functools.partial(hello, param=pin))
-            b.bind('<Double-1>', quit)
+            b.bind('<Button-1>', functools.partial(self.onClick, button=b, pin=pin))
+            b.bind('<Shift-Button-1>', functools.partial(self.onShiftClick, button=b, pin=pin))
+            self.repaintButton(b, pin)
 
         canvasTop.pack()
         self.pack()
@@ -94,14 +97,6 @@ class Zif(Frame):
         else:
             return Zif.zifPosX + Zif.socketWidth + gap
 
-    #
-    # def socketPosH(self, col):
-    #     if col == 0:
-    #         return Zif.marginSide
-    #     else:
-    #         return Zif.socketWidth - Zif.marginSide - Zif.buttonSize
-
-
     def button(self, master, text, height, width, style):
         f = Frame(master, height=height, width=width)
         f.pack_propagate(0) # don't shrink
@@ -116,9 +111,54 @@ class Zif(Frame):
         f.pack_propagate(0) # don't shrink
         f.pack()
 
-        b = Label(f, text=text, style='W.TButton')
-        b.pack(fill=BOTH, expand=1)
-        return (f,b)
+        o = Label(f, text=text, style=style)
+        o.pack(fill=BOTH, expand=1)
+        return (f,o)
+
+    def entry(self, master, text, height, width, font,textvariable):
+        f = Frame(master, height=height, width=width)
+        f.pack_propagate(0) # don't shrink
+        f.pack()
+
+        o = Entry(f, font=font, textvariable=textvariable, justify=CENTER)
+        o.insert(tk.END, text)
+        o.pack(fill=BOTH, expand=1)
+        return (f,o)
+
+
+    def onClick(self, event, button, pin):
+        print("Single Click, Button-%s pin %d for %s" % (button, pin, event))
+        label = self.pinList[pin]
+        idx = self.pinListIdx[pin];
+        idx = (idx + 1) % len(self.options)
+        self.pinListIdx[pin] = idx
+        self.repaintButton(button, pin)
+
+    def onShiftClick(self, event, button, pin):
+        print("Shift Single Click, Button-%s pin %d for %s" % (button, pin, event))
+        label = self.pinList[pin]
+        idx = self.pinListIdx[pin];
+        idx = (idx - 1) % len(self.options)
+        self.pinListIdx[pin] = idx
+        self.repaintButton(button, pin)
+
+    def repaintButton(self, button, pin):
+        idx = self.pinListIdx[pin];
+        code = Zif.options[idx]
+        button.configure(text = code)
+        self.repaintPattern()
+
+    def repaintPattern(self):
+        pattern=""
+        for pin in range(0, len(self.pinListIdx)):
+            idx = self.pinListIdx[pin];
+            code = Zif.options[idx]
+            pattern = pattern + code
+            if pin == (self.pins/2)-1:
+                pattern = pattern + "/"
+        self.testPattern.set(pattern)
+        # self.testPattern.delete('0','end')
+        # self.testPattern.insert(tk.END, pattern)
 
 def main():
 
@@ -127,7 +167,7 @@ def main():
     root.title("Exploratory Tool")
 
     ex = Zif(root)
-    ex.pack(side=LEFT,padx=20, pady=20)
+    ex.pack(side=LEFT)#,padx=20, pady=20)
 
 
     # ex.place(y=60)
