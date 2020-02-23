bfa5c937facaa56e63cb53941c7966c2b31f2ada
John Lonergan <john.lonergan@gmail.com>
2020-02-23 21:06:48 +0000

    form tidied - serial mon cleaned up

--- gui.py
+++ gui.py
@@ -3,31 +3,32 @@ from tkinter import *
 import functools
 from tkinter.ttk import *
 
+from serialMonitor import TesterInterface, loadDefault, saveDefault
 
 def quit(event):
     print("Double Click, so let's stop")
-    import sys; sys.exit()
+    import sys;
+    sys.exit()
 
 
 class Zif(Frame):
-
     socketWidth = 200
 
-    pins=24
-    rows=pins/2
+    pins = 24
+    rows = pins / 2
 
     pitchVert = 30
 
-    marginSide=20
-    marginTop=100
-    marginBot=50
+    marginSide = 20
+    marginTop = 100
+    marginBot = 50
 
     labelSize = 30
 
     pinHeight = 25
     pinWidth = 50
 
-    selectorSize = 50
+    selectorSize = 25
     selectorHeight = 25
     patternHeight = 30
 
@@ -38,45 +39,44 @@ class Zif(Frame):
 
     zifPosX = 100
     zifPosY = 0
-    options = ["?","0","1","V","G","C","L","H","Z","X","S"]
+    options = ["?", "0", "1", "V", "G", "C", "L", "H", "Z", "X", "S"]
 
-    surfaceCol="#F7F7F7"
+    surfaceCol = "#F7F7F7"
 
     def __init__(self, parent, *args, **kwargs):
         self.pinLabels = []
         self.pinCodes = []
-        self.testPattern=StringVar()
-        self.autoTest=False
+        self.testPattern = StringVar()
+        self.autoTest = BooleanVar()
 
         tk.Frame.__init__(self, parent)
         self.initUI()
-
 
     def initUI(self):
 
         self.config(height=Zif.height, width=Zif.width, background="white", borderwidth=0, highlightthickness=0)
 
-        canvasTop = Canvas(self, height=Zif.height+50, width=Zif.width*2, background="white", borderwidth=0, highlightthickness=0)
+        canvasTop = Canvas(self, height=Zif.height + 50, width=Zif.width * 2, background="white", borderwidth=1,
+                           highlightthickness=0)
         canvasTop.pack(side=LEFT, padx=10, pady=20)
 
-        canvas1 = Canvas(canvasTop, height=Zif.height, width=Zif.socketWidth,  background="white", borderwidth=1, highlightthickness=0)
-        canvas1.place(x=Zif.zifPosX, y=Zif.zifPosY)
-        canvas1.create_rectangle(2, 55, Zif.socketWidth, Zif.height, width=1, outline="black", fill=Zif.surfaceCol);
-        canvas1.create_rectangle(20, 20, 40, 80, width=1, outline="black", fill="#EEE");
-        canvas1.create_rectangle(18, 5, 42, 30, width=1, outline="black", fill="#EEE");
+        canvasTop.create_rectangle(Zif.zifPosX+2, Zif.zifPosY+55, Zif.zifPosX+Zif.socketWidth, Zif.zifPosY+Zif.height, width=1, outline="black", fill=Zif.surfaceCol);
+        canvasTop.create_rectangle(Zif.zifPosX+20, Zif.zifPosY+20, Zif.zifPosX+40, Zif.zifPosY+80, width=1, outline="black", fill="#EEE");
+        canvasTop.create_rectangle(Zif.zifPosX+18, Zif.zifPosY+5, Zif.zifPosX+42, Zif.zifPosY+30, width=1, outline="black", fill="#EEE");
 
         self.patternField(canvasTop)
         self.testButton(canvasTop)
+        self.autoCheckbox(canvasTop)
 
         for pin in range(0, 24):
-            self.label(canvasTop, x=self.labelPosH(pin), y=self.pinPosV(pin), text=str(pin+1), width=Zif.labelSize, height=Zif.labelSize)
+            self.label(canvasTop, x=self.labelPosH(pin), y=self.pinPosV(pin), text=str(pin + 1), width=Zif.labelSize,
+                       height=Zif.labelSize)
 
-            pinLabelVariable=StringVar()
-            pinLabelVariable.set("")
-            self.pinLabels.append(pinLabelVariable)
+            self.pin(canvasTop, x=self.pinPosH(pin), y=self.pinPosV(pin),
+                     width=Zif.pinWidth, height=Zif.pinHeight)
 
-            self.pin(canvasTop, x=self.pinPosH(pin), y=self.pinPosV(pin), textvariable=pinLabelVariable, width=Zif.pinWidth, height=Zif.pinHeight)
-            self.optionMenu(canvasTop, x=self.selectorPosH(pin), y=self.pinPosV(pin), width=Zif.selectorSize, height=Zif.selectorHeight, pin=pin, labelvariable=pinLabelVariable)
+            self.optionMenu(canvasTop, x=self.selectorPosH(pin), y=self.pinPosV(pin),
+                            width=Zif.selectorSize, height=Zif.selectorHeight, pin=pin)
 
         self.repaintPattern()
         canvasTop.pack()
@@ -98,7 +98,7 @@ class Zif(Frame):
             return Zif.zifPosX + Zif.socketWidth - Zif.marginSide - Zif.pinWidth
 
     def labelPosH(self, pin):
-        gap=1
+        gap = 1
         if pin < 12:
             return Zif.zifPosX + Zif.marginSide + Zif.pinWidth + gap
         else:
@@ -107,78 +107,80 @@ class Zif(Frame):
     def selectorPosH(self, pin):
         gap = 20
         if pin < 12:
-            return Zif.zifPosX - Zif.selectorSize -gap
+            return Zif.zifPosX - Zif.selectorSize - gap
         else:
             return Zif.zifPosX + Zif.socketWidth + gap
 
-
     def testButton(self, master):
-        xpos = Zif.zifPosX + Zif.socketWidth + 30
-        self.button(master, x=xpos, y=25, text="Test", height=30, width=50, style='W.TButton')
+        def onClick():
+            self.runTest()
 
-    def button(self, master, x, y, text, height, width, style):
+        xpos = Zif.zifPosX + Zif.socketWidth + 30
+        self.button(master, x=xpos, y=25, text="Test", height=30, width=50, command=onClick, style='W.TButton')
+
+    def autoCheckbox(self, master):
+        xpos = Zif.zifPosX + Zif.socketWidth + 30
+        cb = tk.Checkbutton(master, text="Auto", height=1, width=3, background="white", variable=self.autoTest)
+        cb.place(x=xpos, y=25 + 30)
+
+    def button(self, master, x, y, text, height, width, command, style):
         f = Frame(master, height=height, width=width)
-        f.pack_propagate(0) # don't shrink
+        f.pack_propagate(0)  # don't shrink
         f.pack()
         f.place(x=x, y=y)
 
-        b = Button(f, text=text, style=style)
+        b = Button(f, text=text, style=style, command=command)
         b.pack(fill=BOTH, expand=1)
-        return (f,b)
+        return b
 
-    def optionMenu(self, master, x,y, height, width, pin, labelvariable):
-
-        variable=StringVar()
+    def optionMenu(self, master, x, y, height, width, pin):
 
         def onClick(code):
-            print("pin %d = %s" % (pin, code))
-            labelvariable.set(code)
             self.repaintPattern()
+            if self.autoTest.get():
+                self.runTest()
 
         f = Frame(master, height=height, width=width)
-        f.pack_propagate(0) # don't shrink
+        f.pack_propagate(0)  # don't shrink
         f.pack()
-        f.place(x=x,y=y)
+        f.place(x=x, y=y)
 
-        b = OptionMenu(f, variable, "?", command=onClick, *self.options)
+        variable = StringVar()
+        b = OptionMenu(f, variable, "?", command=onClick, style='W.TButton', *self.options)
+        b["menu"].config( bg="white" , activebackground="red", selectcolor="green")
+        # print(b.keys())
         b.pack(fill=BOTH, expand=1)
 
         self.pinCodes.append(variable)
-        return (f,b)
 
     def label(self, master, x, y, text, height, width):
         f = Frame(master, height=height, width=width)
-        f.pack_propagate(0) # don't shrink
+        f.pack_propagate(0)  # don't shrink
         f.pack()
         f.place(x=x, y=y)
 
         o = Label(f, text=text, font=("courier", 9), background=Zif.surfaceCol, borderwidth=0, anchor="center")
         o.pack(fill=BOTH, expand=1)
 
-    def pin(self, master, x,y,textvariable, height, width):
+    def pin(self, master, x, y, height, width):
         f = Frame(master, height=height, width=width)
-        f.pack_propagate(0) # don't shrink
+        f.pack_propagate(0)  # don't shrink
         f.pack()
-        f.place(x=x,y=y)
+        f.place(x=x, y=y)
 
-        o = Label(f, textvariable=textvariable, font=("courier", 9), background="#DDD", borderwidth=2, relief="solid", anchor="center")
+        pinLabelVariable = StringVar()
+        pinLabelVariable.set("")
+        self.pinLabels.append(pinLabelVariable)
+
+        o = Label(f, textvariable=pinLabelVariable, font=("courier", 9), background="#DDD", borderwidth=2,
+                  relief="solid", anchor="center")
         o.pack(fill=BOTH, expand=1)
-
-    def entry(self, master, text, height, width, font,textvariable):
-        f = Frame(master, height=height, width=width)
-        f.pack_propagate(0) # don't shrink
-        f.pack()
-
-        o = Entry(f, font=font, textvariable=textvariable, justify=CENTER)
-        o.insert(tk.END, text)
-        o.pack(fill=BOTH, expand=1)
-        return (f,o)
 
     def patternField(self, master):
         f = Frame(master, width=Zif.width, height=Zif.patternHeight)
-        f.pack_propagate(0) # don't shrink
+        f.pack_propagate(0)  # don't shrink
         f.pack()
-        f.place(x=Zif.zifPosX, y=Zif.height+20)
+        f.place(x=Zif.zifPosX, y=Zif.height + 20)
 
         o = Entry(f, font=("courier", 9),
                   textvariable=self.testPattern, width=Zif.width,
@@ -187,26 +189,33 @@ class Zif(Frame):
 
         o.configure(style="W.TButton")
 
-        return (f,o)
+        return (f, o)
 
     def repaintPattern(self):
-        pattern=""
+        pattern = ""
         for pin in range(0, 24):
             code = self.pinCodes[pin].get()
             pattern = pattern + code
-            if pin == (self.pins/2)-1:
+            if pin == (self.pins / 2) - 1:
                 pattern = pattern + "/"
         self.testPattern.set(pattern)
 
-def main():
+    def runTest(self):
+        print("testing")
+        self.paintResult("ABCDEFGHIJKLMNlksdjflksdjflksjdflkj")
 
+    def paintResult(self, result):
+        for ipin in range(0,24):
+            self.pinLabels[ipin].set(result[ipin])
+
+
+def main():
     root = Tk()
 
     root.title("Exploratory Tool")
 
     ex = Zif(root)
     ex.pack(side=LEFT)
-
 
     # ex.place(y=60)
     ex.pack()
--- serialMonitor.py
+++ serialMonitor.py
@@ -2,100 +2,169 @@
 from serial import Serial
 import time
 import string
+from pathlib import Path
 import sys
 import threading
 import time
 import platform
 from os import _exit
-import traceback
-import signal
+
+home = str(Path.home())
 
 ## compat
 try:
-  import readline
+    import readline
 except ImportError:
-  import pyreadline as readline
+    import pyreadline as readline
 
-com='5'
-
-print("serial port number [%s] ? " % com)
-c = sys.stdin.read(1)
-if (c.strip() != ""):
-    com = c
-
-if (platform.system() == "Linux"):
-    port = '/dev/ttyS' + com 
-else:
-    port = 'com' + com
-
-print("opening "+ port)
-
-ard = Serial(port,9600,timeout=0.05)
-
-print("opened "+ port)
-
-def keyboardInput():
-
+def loadDefault():
     try:
-        while True:
-            line = sys.stdin.readline().strip()
-            if (line == "q"):
-                break
+        with open(home + "/.ictester") as f:
+            content = f.readlines()
 
-            # write to arduino
-            if len(line) > 0:
-                ard.write(line.encode("utf-8"))
-                ard.write("\n".encode("utf-8"))
-   
-        print("QUIT")
-        _exit(0)
-    except BaseException as x:
-        print("ERR READING STDIN\n")
-        print(x)
-        _exit(1)
+        if len(content)>0:
+            return content[0].strip()
+        else:
+            None
+    except:
+        pass
 
-def readSerial():
-    try:
-        msg = ard.readline()
-        while (1): 
-            resp = msg.decode("utf-8")#.strip()
-            if (len(resp) > 0):
-                print("%s" % resp, end = '')
-                sys.stdout.flush()
-            msg = ard.readline()
-    except BaseException as x:
-        print("ERR ON SERIAL\n")
-        print(x)
-        _exit(1)
-    
-def startThreads(ard):
+def saveDefault(comNum):
+    with open(home + "/.ictester", "w") as f:
+        f.write(comNum)
 
-    # thread to read local keyboard input
-    input_thread = threading.Thread(target=keyboardInput)
-    input_thread.daemon = False
-    input_thread.start()
 
-    # thread to read and print data from arduino
-    sinput_thread = threading.Thread(target=readSerial)
-    sinput_thread.daemon = True
-    sinput_thread.start()
-    
-startThreads(ard)
+def selectPort():
+    default = loadDefault()
+
+    print("serial port number [%s] ? " % default, end="")
+    sys.stdout.flush()
+
+    c = sys.stdin.readline().strip()
+    if c.strip() != "":
+        com = c
+    elif default:
+        com=default
+    else:
+        print("must select port")
+        return selectPort()
+
+    saveDefault(com)
+
+    if com.isdigit():
+        if (platform.system() == "Linux"):
+            port = '/dev/ttyS' + com
+        else:
+            port = 'com' + com
+    else:
+        port = com
+
+    print("port is " + port)
+    return port
+
+class TesterInterface():
+
+    def _recvResponse(resp):
+        print("%s" % resp, end='')
+
+    def __init__(self, serialPort, responseHandler = _recvResponse, *args, **kwargs):
+
+        self.responseHandler = responseHandler
+        self.ard = None
+
+        self.port = serialPort
+
+    def open(self):
+        print("opening " + self.port)
+        self.ard = Serial(self.port, 9600, timeout=0.05)
+        print("opened " + self.port)
+        self._startThreads()
+
+    def close(self):
+        self.ard.close()
+        sys.stdin.close()
+
+    def _keyboardInput(self):
+        try:
+            while True:
+                line = sys.stdin.readline().strip()
+                if (line == "q"):
+                    break
+
+                # write to arduino
+                if len(line) > 0:
+                    self.writeToTester(line)
+
+            print("QUIT")
+            _exit(0)
+        except BaseException as x:
+            print("ERR READING STDIN\n")
+            print(x)
+            _exit(1)
+
+    def _readSerial(self):
+        try:
+            msg = self.ard.readline()
+            while (1):
+                resp = msg.decode("utf-8")  # .strip()
+                if (len(resp) > 0):
+                    self.responseHandler(resp)
+                    sys.stdout.flush()
+                msg = self.ard.readline()
+        except BaseException as x:
+            print("ERR ON SERIAL\n")
+            print(x)
+            _exit(1)
+
+    def _startThreads(self):
+
+        if not self.ard:
+            raise Exception("Tester port not open")
+
+        # thread to read local keyboard input
+        input_thread = threading.Thread(target=self._keyboardInput)
+        input_thread.daemon = False
+        input_thread.start()
+
+        # thread to read and print data from arduino
+        sinput_thread = threading.Thread(target=self._readSerial)
+        sinput_thread.daemon = True
+        sinput_thread.start()
+
+    def writeToTester(self, testcase):
+        self.ard.write(testcase.encode("utf-8"))
+        self.ard.write("\n".encode("utf-8"))
+
 
 
 # Support for CTRL-C needs main thread still running.
 # This is actually a crucial step, otherwise all signals sent to your program will be ignored.
 # Adding an infinite loop using time.sleep() after the threads have been started will do the trick: 
-try:
-    while True:
-        time.sleep(1) 
-except KeyboardInterrupt:
-        _exit(1)   
-except BaseException as x:
+def recvResponse(resp):
+    print("%s" % resp, end='')
+
+
+def main():
+    com = selectPort()
+
+    tester = TesterInterface(serialPort=com)
+
     try:
-        print("ERR3 %s" % x)
-        ard.close()
-        sys.stdin.close()
-        _exit(1)   
-    except:
-        _exit(1)   
+        tester.open();
+
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        _exit(1)
+    except BaseException as x:
+        try:
+            print("ERR3 %s" % x)
+            tester.close()
+            sys.stdin.close()
+            _exit(1)
+        except:
+            _exit(1)
+
+
+if __name__ == '__main__':
+    main()
